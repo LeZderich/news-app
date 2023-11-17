619c33f42566c28478cf35ef09e4015ff888ad9a
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-11-17 14:29:28 +0100

    Link the articles in a balise in the index

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,9 +1,8 @@
 <div>
-    <h1>Articles</h1>
+    ss<h1>Articles</h1>
     @foreach($articles as $article)
         <div>
-            <h2>{{$article->title}}</h2>
-            <p>{{$article->body}}</p>
+            <h2><a href="{{route('articles.show', $article)}}">{{$article->title}}</a></h2>
         </div>
     @endforeach
 </div>

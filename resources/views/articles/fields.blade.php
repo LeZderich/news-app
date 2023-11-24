d4fa31f78ecc3aed9681c3aa6651aa62a0425c57
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-11-24 14:26:59 +0100

    Implement layout

---
+++ resources/views/articles/fields.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <label for="title">Titre</label>
+    <input type="text" name="title" id="title" value="{{old('title')}}">
+
+</div>
+<div>
+    <label for="body">Contenu</label>
+    <textarea name="body" id="body" cols="30" rows="10">{{old('body')}}</textarea>
+
+</div>
+<button type="submit" value="Create">Créer</button>
